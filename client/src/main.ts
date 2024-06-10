09a96040eef646f691f376e6af68b6607d0a324c
Heunsig <heun3344@gmail.com>
2024-06-10 22:36:21 +0900

    chore: set up tanstack query

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,7 @@ import "./main.css";
 import App from "./App.vue";
 import { router } from "./router";
 import PrimeVue from "primevue/config";
+import { VueQueryPlugin } from "@tanstack/vue-query";
 import "primevue/resources/themes/aura-dark-noir/theme.css";
 
-createApp(App).use(PrimeVue).use(router).mount("#app");
+createApp(App).use(PrimeVue).use(VueQueryPlugin).use(router).mount("#app");
